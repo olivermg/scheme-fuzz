693458a7a50fbb4e791b71d8941bed1333a400cb
Oliver Wegner <void1976@gmail.com>
2012-08-25 12:59:12 +0200

    Introducing fuzzy-set as struct

--- fuzzy.rkt
+++ fuzzy.rkt
@@ -1,7 +1,12 @@
 #lang racket
 
-(define (make-fuzzy-set fn offset scale cog)
-  (lambda (x)
-    (let ([effective-x (- x offset)])
-      (* (fn effective-x)
-         scale))))
+(struct fuzzy-set (calc cog))
+
+(define (make-fuzzy-set fn shift scale weight)
+  (fuzzy-set
+   (lambda (x)
+     (let ([effective-x (- x shift)])
+       (* (fn effective-x)
+          scale)))
+   (lambda ()
+     (* scale weight))))
